2c792da1bd8a7f243a8d304b7f4bcc36946f562f
Ivo van der Wijk <github@in.m3r.nl>
2016-06-03 11:49:48 +0200

    remove m3r name

---
+++ cubric/recipes/django_rest_deploy.py
@@ -0,0 +1,146 @@
+import os
+
+from cubric import DeploymentBase, Template, Git, Venv, Supervisor
+from cubric import NGINX, File, BaseConfig, Postgres, Ubuntu
+
+pj = os.path.join  # gives us local style joining, may be different from remote
+"""
+http://jessenoller.com/blog/2009/02/05/ssh-programming-with-paramiko-completely-different
+
+Use Path module for path manipulation? (in stead of pj hack)
+"""
+
+
+class DRFProjectDeployment(DeploymentBase):
+    # local install wkhtmltopdf
+
+    requires = (Venv, Template, Supervisor, File, NGINX, Git, Postgres,
+                Ubuntu)
+
+    def setup_database(self, env):
+        with env.sudo("postgres"):
+            self.postgres.user(self.config.db_user, self.config.db_pass)
+            self.postgres.database(self.config.db_name, self.config.db_user)
+
+    def git_buildout(self, env):
+        self.git.cloneup(self.config.repo, self.config.project)
+
+        env.chdir(self.config.project)
+        self.venv.create() \
+            .create() \
+            .install("zc.buildout==2.5.0") \
+            .install("setuptools")
+
+        env.command("bin/buildout", "-c", "development.cfg")
+
+    def django_update(self, env):
+        """ update database, etc """
+        # XXX Wrap this in some command, with collectstatic, etc
+        env.chdir(pj(self.config.instancepath, self.config.project))
+        env.command("bin/django", "migrate", "--noinput")
+
+    def setup_huey(self, env):
+        # install redis
+        # config redis
+        # setup supervisor config for huey
+        Ubuntu(env).install("redis-server")
+        svhueypath = pj(self.config.instancepath, "supervisor-huey.conf")
+        command = pj(self.config.instancepath, self.config.project,
+                     "bin", "django") + " run_huey"
+
+        self.template \
+            .create(src="templates/supervisor-program.conf",
+                    dst=svhueypath,
+                    args=self.config,
+                    program="run_huey",
+                    command=command)
+        self.supervisor.install(svhueypath, "{0}-huey".format(
+            self.config.projectid))
+
+    def setup_celery(self, env):
+        with env.sudo():
+            # these commands "fail" if the user / vhost already exist, hence
+            # the /bin/true hack which will always give a positive result.
+            _env = self.config.environment
+
+            env.command("rabbitmqctl", "add_user", _env, _env, nonzero=True)
+            env.command("rabbitmqctl", "add_vhost", _env, nonzero=True)
+            env.command("rabbitmqctl", "set_permissions", "-p", _env, _env,
+                        '".*"', '".*"', '".*"')
+        svcdpath = pj(self.config.instancepath, "supervisor-celeryd.conf")
+        svcbpath = pj(self.config.instancepath, "supervisor-celerybeat.conf")
+        self.template \
+            .create(src="templates/supervisor-celeryd.conf",
+                    dst=svcdpath,
+                    args=self.config) \
+            .create(src="templates/supervisor-celerybeat.conf",
+                    dst=svcbpath,
+                    args=self.config)
+        self.supervisor.install(svcdpath, "{0}-celeryd".format(
+            self.config.projectid))
+        self.supervisor.install(svcbpath, "{0}-celerybeat".format(
+            self.config.projectid))
+
+    def run(self, env):
+
+        self.setup_database(env)
+
+        env.set("DATABASE_URL", self.config.database_url)
+        env.set("BROKER_URL",
+                "ampq://{env}:{env}:5672/{env}"
+                .format(env=self.config.environment))
+
+        with env.sudo():
+            env.mkdir(self.config.instancepath, chdir=True,
+                      owner=self.config.user)
+
+        self.git_buildout(env)
+
+        self.django_update(env)
+
+        env.chdir(self.config.instancepath)
+
+        for d in ("logs", "run", "static", "media", "www"):
+            env.mkdir(pj(self.config.instancepath, d))
+        with env.sudo():
+            self.file.present(pj(self.config.instancepath, "run"),
+                              type=File.DIR,
+                              mode="2770",
+                              group="www-data")
+        # chaining makes it impossible (?) to do conditional stuff.
+        # unless it becomes an extra step in the chain:
+        # if tpl.create(...).changed():
+        #  .. do stuff if changed
+        svcpath = pj(self.config.instancepath, "supervisor.conf")
+        nginxpath = pj(self.config.instancepath, "nginx.conf")
+        uwsgiconfpath = pj(self.config.instancepath, "uwsgi.conf")
+        uwsgicommand = "{0} --ini {1}".format(
+                       self.config.uwsgi, uwsgiconfpath)
+        self.template \
+            .create(src="templates/supervisor-program.conf",
+                    dst=svcpath,
+                    args=self.config,
+                    command=uwsgicommand,
+                    program="uwsgi") \
+            .create(src="templates/uwsgi.conf",
+                    dst=pj(self.config.instancepath, "uwsgi.conf"),
+                    args=self.config) \
+            .create(src="templates/nginx.conf",
+                    dst=nginxpath,
+                    args=self.config)
+        self.supervisor.install(svcpath, "{0}-uwsgi".format(
+            self.config.projectid))
+
+        # self.setup_celery(env)
+        self.setup_huey(env)
+
+        self.nginx.install(nginxpath, self.config.projectid)
+
+
+class DRFProjectConfig(BaseConfig):
+    environment = "dev"
+    projectname = "acmeproject"
+
+    @property
+    def projectid(self):
+        return "{0}-{1}".format(self.projectname, self.environment)
---
+++ cubric/recipes/emberproject.py
@@ -0,0 +1,54 @@
+from cubric import DeploymentBase
+from cubric import Git, File, Template, NGINX
+from path import Path
+import plumbum
+import datetime
+
+
+class EmberLocalBuild(DeploymentBase):
+    requires = (Git, )
+
+    def sync_repo(self, env):
+        self.git.cloneup(self.config.repo, self.config.project)
+
+    def run(self, env):
+        self.sync_repo(env)
+        with env.chdir(self.config.project):
+            print("Using node:")
+            env.command("node", "-v")
+            env.command("npm", "install")
+            env.command("bower", "install")
+            # env.command("ember", "build", "--env=production")
+            env.command("ember", "build", "--env=demo")
+
+
+class EmberDeploy(DeploymentBase):
+    requires = (Git, File, Template, NGINX)
+
+    def run(self, env):
+        """
+            - Create remote instance folder
+            - Create folder based on commit hash
+            - Recursive copy dist/
+            - Symlink
+            - Setup / update nginx
+        """
+
+        env.mkdir(self.config.instancepath, chdir=True)
+        # Not usable, works remotely not local!
+        # print("Hash", self.git.shorthash(self.config.project))
+
+        tsfolder = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        for d in ("logs", tsfolder):
+            env.mkdir(self.config.instancepath / d)
+        plumbum.path.utils.copy(Path(self.config.project) / 'dist',
+                                env.host.path(self.config.instancepath
+                                              / tsfolder))
+        self.file.present(self.config.instancepath / tsfolder,
+                          target=self.config.instancepath / 'www',
+                          type=File.LINK)
+        self.template.create(src="templates/emberapp-nginx.conf",
+                             dst=self.config.instancepath / 'nginx.conf',
+                             args=self.config)
+        self.nginx.install(self.config.instancepath / 'nginx.conf',
+                           self.config.projectid)
