f67bd5b492eb7c6704c17d13851109c9623204d9
Ivo van der Wijk <github@in.m3r.nl>
2017-05-19 15:57:37 +0200

    disable BROKER_URL env

--- cubric/recipes/django_rest_deploy.py
+++ cubric/recipes/django_rest_deploy.py
@@ -106,9 +106,10 @@ class DRFProjectDeployment(DeploymentBase):
         env.set("DATABASE_URL", self.config.database_url)
         env.set("PROJECT_HOME", pj(self.config.instancepath,
                                    self.config.project))
-        env.set("BROKER_URL",
-                "ampq://{env}:{env}:5672/{env}"
-                .format(env=self.config.environment))
+        # for celery
+        # env.set("BROKER_URL",
+        #         "ampq://{env}:{env}:5672/{env}"
+        #         .format(env=self.config.environment))
 
         with env.sudo():
             env.mkdir(self.config.instancepath, chdir=True,
